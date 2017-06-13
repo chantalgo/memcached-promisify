ab0410adc529d23e60e5b35b29a6f282b11e78ac
Chris Wood <chris.wood@flightstats.com>
2017-06-13 13:00:33 -0700

    Removed bluebird in favor of native promises. Some ES6 tweaks. Revved to major version.

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,9 @@
-'use strict';
-
-var expect = require('chai').expect;
-var rewire = require('rewire');
+const _get = require('lodash.get');
+const _isempty = require('lodash.isempty');
 const assert = require('assert');
-const _ = require('lodash');
-var Cache = rewire('../index.js');
+const expect = require('chai').expect;
+const rewire = require('rewire');
+const Cache = rewire('../index.js');
 
 /*
  * cache mockers
@@ -17,10 +16,10 @@ MockCacheRejected.prototype.set = function (key, value, exp, cb) {
   return cb('Test set rejection');
 };
 
-describe('cache', function() {
-  var cache = new Cache({ keyPrefix: 'test' }, { maxExpiration: 900 });
+describe('cache', () => {
+  const cache = new Cache({ keyPrefix: 'test' }, { maxExpiration: 900 });
 
-  it('should have various methods', function(){
+  it('should have various methods', () => {
     expect(cache).to.respondTo('get');
     expect(cache).to.respondTo('utilGet');
     expect(cache).to.respondTo('getMulti');
@@ -29,106 +28,105 @@ describe('cache', function() {
     expect(cache).to.respondTo('stats');
   });
 
-  describe('execute methods', function() {
+  describe('execute methods', () => {
 
-
-    it ('should return stats response with non-zero number of items', function(done) {
-      cache.set('herp', 'derp', 20)
-          .then(function (setResult1) {
-            return cache.stats();
-          })
-          .then(function (stats) {
-            assert.ok(!_.isEmpty(stats));
-            assert.ok(_.get(stats, '0.total_items', 0) > 0);
-            done();
-          })
-    })
-
-    it('should set/get something in the cache', function(done) {
+    it('should set/get something in the cache', (done) => {
       cache.set('foo', 'bar', 20)
-        .then(function (setResult1) {
+        .then((setResult1) => {
           return cache.get('foo');
         })
-        .then(function (getResult1) {
+        .then((getResult1) => {
           expect(getResult1).to.eq('bar');
           return cache.set('foo', 'bas', 20);
         })
-        .then(function (setResult2) {
+        .then((setResult2) => {
           return cache.get('foo');
         })
-        .then(function (getResult2) {
+        .then((getResult2) => {
           expect(getResult2).to.eq('bas');
           done();
         });
     });
 
-    it('should set/utilGet something in the cache', function(done) {
+    it('should set/utilGet something in the cache', (done) => {
       cache.set('foo', 'bar', 20)
-        .then(function (setResult) {
+        .then((setResult) => {
           return cache.utilGet('test' + 'foo');
         })
-        .then(function (utilGetResult) {
+        .then((utilGetResult) => {
           expect(utilGetResult).to.eq('bar');
           done();
-        }, function (utilGetError) {
+      }, (utilGetError) => {
           throw new Error(utilGetError);
         });
     });
 
-    it('should set/multiGet something in the cache', function(done) {
+    it('should set/multiGet something in the cache', (done) => {
       cache.set('foo', 'bar', 20)
-        .then(function (setResult1) {
+        .then((setResult1) => {
           return cache.set('fos', 'bas', 20);
         })
-        .then(function (setResult2) {
+        .then((setResult2) => {
           return cache.getMulti(['testfoo', 'testfos']);
         })
-        .then(function (getMultiResult) {
+        .then((getMultiResult) => {
           expect(getMultiResult.testfoo).to.eq('bar');
           expect(getMultiResult.testfos).to.eq('bas');
           done();
         });
     });
 
-    it('should del something in the cache', function(done) {
+    it ('should return stats response with non-zero number of items', (done) => {
+      cache.set('herp', 'derp', 20)
+        .then((setResult1) => {
+          return cache.stats();
+        })
+        .then((stats) => {
+          assert.ok(!_isempty(stats));
+          assert.ok(_get(stats, '0.total_items', 0) > 0);
+          done();
+        });
+    });
+
+    it('should del something in the cache', (done) => {
       cache.set('foo', 'bar', 20)
-        .then(function (setResult) {
+        .then((setResult) => {
           return cache.del('foo');
         })
-        .then(function (delResult) {
+        .then((delResult) => {
           return cache.get('foo');
-        }, function (delError) {
+      }, (delError) => {
           throw delError;
         })
-        .then(function (getResult) {
+        .then((getResult) => {
           done();
-        }, function (getError) {
+      }, (getError) => {
           throw getError;
         });
     });
 
-    it('should return undefined if attempting to get something that does not exist', function(done) {
-      var keyName = 'test-' + Date.now().toString();
+    it('should return undefined if attempting to get something that does not exist', (done) => {
+      const keyName = `test-${Date.now().toString()}`;
       cache.get(keyName)
-        .then(function (getResult) {
+        .then((getResult) => {
           done();
-        }, function (getError) {
+      }, (getError) => {
           throw getError;
         });
     });
 
-    it('should return false if attempting to del something that does not exist', function(done) {
-      var keyName = 'test-' + Date.now().toString();
+    it('should return false if attempting to del something that does not exist', (done) => {
+      const keyName = `test-${Date.now().toString()}`;
       cache.del(keyName)
-        .then(function (delResult) {
+        .then((delResult) => {
           done();
-        }, function (delError) {
+      }, (delError) => {
           throw delError;
         });
     });
 
-    it('should limit the expiration value', function(done){
-      expect(function(){
+    it('should limit the expiration value', (done) => {
+      expect(() => {
         cache.set('foo', 'bar', 10000000)
           .then(function (result) {},
           function (err) {});
@@ -138,27 +136,27 @@ describe('cache', function() {
 
   });
 
-  describe('mock rejections', function() {
+  describe('mock rejections', () => {
 
     Cache.__with__({
       'Memcached': MockCacheRejected
-    })(function () {
-      var cacheRejected = new Cache();
+    })(() => {
+      const cacheRejected = new Cache();
 
-      it('get', function (done) {
+      it('get', (done) => {
         cacheRejected.get('test')
-          .then(function (resp) {
+          .then((resp) => {
             throw new Error('Succeeded but expected fail');
-          }, function (err) {
+        }, (err) => {
             done();
           });
       });
 
-      it('set', function (done) {
+      it('set', (done) => {
         cacheRejected.set('test', 'this', 1000)
-          .then(function (resp) {
+          .then((resp) => {
             throw new Error('Succeeded but expected fail');
-          }, function (err) {
+        }, (err) => {
             done();
           });
       });
